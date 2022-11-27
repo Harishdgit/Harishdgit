d1d6e57f0c00c72897383e3109fb48ea61749c9c
harishhampapati <harishhampapati@gmail.com>
2022-11-27 17:52:43 +0530

    Change in the SubBranch

--- src/main/java/com/testQa/MergeTest/CheckLastTime.java
+++ src/main/java/com/testQa/MergeTest/CheckLastTime.java
@@ -5,6 +5,7 @@ public class CheckLastTime {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("New Changes are added");
+		System.out.println("The change in the Eclipse sub branch");
 	}
 
 }
