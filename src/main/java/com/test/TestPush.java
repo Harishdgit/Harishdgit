63eecd5056e7e71381e04874a1089bcbd40516c1
Harishdgit <62867411+Harishdgit@users.noreply.github.com>
2022-11-27 15:48:40 +0530

    Update TestPush.java

--- src/main/java/com/test/TestPush.java
+++ src/main/java/com/test/TestPush.java
@@ -5,6 +5,8 @@ public class TestPush {
 	public static void main(String[] args) {
 		System.out.println("Hash");
 		// TODO Auto-generated method stub
+		
+		System.out.println("Hashnew code");
 
 	}
 
