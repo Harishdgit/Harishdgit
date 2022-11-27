475f4505b7d2a523ccf389e5e954a1b36889df7b
harishhampapati <harishhampapati@gmail.com>
2022-11-27 17:24:05 +0530

    TestIt

---
+++ src/main/java/com/testQa/MergeTest/NewBrachAsitis.java
@@ -0,0 +1,10 @@
+package com.testQa.MergeTest;
+
+public class NewBrachAsitis {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Has to be tested");
+	}
+
+}
