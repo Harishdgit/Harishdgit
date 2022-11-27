b3266b4b04bf689fe24a7ccd3d9b046a9f37c53c
Harishdgit <62867411+Harishdgit@users.noreply.github.com>
2022-11-27 17:53:00 +0530

    Update CheckLastTime.java

--- src/main/java/com/testQa/MergeTest/CheckLastTime.java
+++ src/main/java/com/testQa/MergeTest/CheckLastTime.java
@@ -5,7 +5,7 @@ public class CheckLastTime {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("New Changes are added");
-		System.out.println("In Master making the changes");
+		System.out.println("In Master making the changes as");
 		
 	}
 
